e7914aad015bceb7502cf142023378099cf50a18
David Ha <David.ha0908@gmail.com>
2021-11-26 11:16:08 -0700

    Updates

---
+++ _nuxt/static/1637950566/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcpnt200-a4\u002F_nuxt\u002Fstatic\u002F1637950566",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcpnt200-a4\u002F",assetsPath:"\u002Fcpnt200-a4\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f2c8d01e"}}}}(null));
