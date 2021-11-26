b7191e0168374163a0b5211965b3f50cddc9ddea
David Ha <David.ha0908@gmail.com>
2021-11-26 11:19:37 -0700

    tweaks

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/cpnt200-a4/'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'cpnt200-a4',
